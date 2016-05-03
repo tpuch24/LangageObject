6b466d986bb685b24699eae93f97c2c5b3090b9c
tp24 <t.puch@laposte.net>
2016-05-03 10:30:01 +0200

    first commit on Master

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,11 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		displaySentence3();
 
 	}
-
+	private static void displaySentence3(){
+		System.out.println("Affichage 3");
+	}
 }
