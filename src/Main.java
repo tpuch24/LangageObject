8231464a12abd20b8c04a45a21060d89886b54c7
tp24 <t.puch@laposte.net>
2016-05-03 10:27:24 +0200

    commit 2 on testing1

--- src/Main.java
+++ src/Main.java
@@ -7,11 +7,15 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		displaySentence1();
+		displaySentence2();
 		
 	}
 
 	private static void displaySentence1(){
 		System.out.println("Affichage 1");
+	}
+	
+	private static void displaySentence2(){
+		System.out.println("Affichage 2");
 	}
 }
